d38d4ac535a1581f522e2ffcca1928ea5a73fb85
Anupa Isuru <anupaisuru2002@gmail.com>
2024-02-01 09:01:25 +0530

    create profile page

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,9 +1,48 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
+import { useSelector } from "react-redux";
 
 function Profile() {
+  const { currentUser, loading, error } = useSelector((state) => state.user);
   return (
-    <div>Profile</div>
-  )
+    <div className="max-w-lg p-3 mx-auto">
+      <h1 className="text-3xl font-semibold text-center my-7">Profile</h1>
+      <form className="flex flex-col gap-4">
+        <img
+          src={currentUser.avatar}
+          alt="profile"
+          className="self-center object-cover w-24 h-24 mt-2 rounded-full cursor-pointer"
+        />
+        <input
+          type="text"
+          placeholder="username"
+          defaultValue={currentUser.username}
+          id="username"
+          className="p-3 border rounded-lg"
+        />
+        <input
+          type="email"
+          placeholder="email"
+          defaultValue={currentUser.email}
+          id="email"
+          className="p-3 border rounded-lg"
+        />
+        <input
+          type="password"
+          placeholder="password"
+          id="password"
+          className="p-3 border rounded-lg"
+        />
+        <button className="p-3 text-white uppercase rounded-lg bg-slate-700 hover:opacity-95 disabled:opacity-80">
+          Update
+        </button>
+      </form>
+      <div className="flex justify-between mt-5">
+        <span className="text-red-700 cursor-pointer">Delete account</span>
+        <span className="text-red-700 cursor-pointer">Sign out</span>
+      </div>
+    </div>
+  );
 }
 
-export default Profile
+export default Profile;
