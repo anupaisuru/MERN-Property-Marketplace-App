995431ac69ee251dd0b8f337fc613453a377a2ed
Anupa Isuru <anupaisuru2002@gmail.com>
2024-02-04 15:04:29 +0530

    complete listing page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,9 +4,11 @@ import About from "./pages/About";
 import Profile from "./pages/Profile";
 import SignIn from "./pages/SignIn";
 import SignUp from "./pages/SignUp";
+import UpdateListing from "./pages/UpdateListing";
 import Header from "./components/Header";
 import PrivateRoute from "./components/PrivateRoute";
 import CreateListing from "./pages/CreateListing";
+import Listing from "./pages/Listing";
 
 function App() {
   return (
@@ -18,9 +20,14 @@ function App() {
           <Route path="/about" element={<About />} />
           <Route path="/sign-in" element={<SignIn />} />
           <Route path="/sign-up" element={<SignUp />} />
+          <Route path="/listing/:listingId" element={<Listing />} />
           <Route element={<PrivateRoute />}>
             <Route path="/profile" element={<Profile />} />
             <Route path="/create-listing" element={<CreateListing />} />
+            <Route
+              path="/update-listing/:listingId"
+              element={<UpdateListing />}
+            />
           </Route>
         </Routes>
       </BrowserRouter>
---
+++ client/src/components/ListingItem.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function ListingItem() {
+  return <div>ListingItem</div>;
+}
+
+export default ListingItem;
