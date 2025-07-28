ac914196ab506c463ebf3d9896d988b37bb2c9bc
anupaisuru <anupaisuru2002@gmail.com>
2025-07-28 11:19:46 +0530

    add cors library for the backend

--- server/server.js
+++ server/server.js
@@ -2,9 +2,11 @@ import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 dotenv.config();
+import cors from "cors"
 import cookieParser from "cookie-parser";
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 app.use(cookieParser());
 
